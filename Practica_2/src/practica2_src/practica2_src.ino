e92421df6c1c52bb844eaf3e5d6cfb8120ae6534
Roberto Osorno <69988498+ElJuanNieves@users.noreply.github.com>
2024-09-15 12:45:06 -0600

    Update practica2_src.ino

--- Practica_2/src/practica2_src/practica2_src.ino
+++ Practica_2/src/practica2_src/practica2_src.ino
@@ -1,25 +1,89 @@
-#ifdef __cplusplus
-extern "C" {
-#endif
-uint8_t temprature_sens_read();
-#ifdef __cplusplus
-}
-#endif
-uint8_t temprature_sens_read();
+#include <WiFiClient.h>
+
+const char* ssid = "Mi_WiFi";
+const char* password = "Mi_Password";
+WiFiServer server(80);
+
+// Se tiene un LED incorporado en terminal 2, se puede usar otro GPIO
+#define LED 2
+
+String estado = "";
 
 void setup() {
-  pinMode(2, OUTPUT);
-  Serial.begin(9600);
+  Serial.begin(115200);
+  pinMode(LED, OUTPUT);
+  digitalWrite(LED, LOW);
+
+  // Conectando a WiFi
+  WiFi.begin(ssid, password);
+
+  // Checa si esta conectado
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi conectado");
+  Serial.print("La dirección IP es: ");
+  Serial.println(WiFi.localIP()); // Muestra IP
+
+  // Inicio del Servidor web.
+  server.begin();
+  Serial.println("Servidor web iniciado.");
 }
 
 void loop() {
-  digitalWrite(2,HIGH);
-  Serial.println("Led prendido");
-  delay(2500);
-  digitalWrite(2,LOW);
-  Serial.println("Led Apagado");
-  delay(2500);
-  Serial.println("La temperatura interna del ESP32 es: ");
-  Serial.println((temprature_sens_read()-32)/1.8);
-  Serial.println("");
+  // Consulta si se ha conectado algún cliente.
+  WiFiClient client = server.available();
+  if (!client) {
+    return;
+  }
+
+  Serial.print("Nuevo cliente: ");
+  Serial.println(client.remoteIP());
+
+  // Espera hasta que el cliente envíe datos.
+  while (!client.available()) {
+    delay(1);
+  }
+
+  // Lee la información enviada por el cliente.
+  String req = client.readStringUntil('\r');
+  Serial.println(req);
+
+  // Realiza la petición del cliente.
+  if (req.indexOf("on2") != -1) {
+    digitalWrite(LED, HIGH);
+    estado = "Encendido";
+  }
+
+  if (req.indexOf("off2") != -1) {
+    digitalWrite(LED, LOW);
+    estado = "Apagado";
+  }
+
+  client.println("HTTP/1.1 200 OK");
+  client.println("Content-Type: text/html");
+  client.println(""); // Importante.
+  client.println("<!DOCTYPE HTML>");
+  client.println("<html>");
+  client.println("<head><meta charset=utf-8></head>");
+  client.println("<body><center><font face='Arial'>");
+  client.println("<h1>Servidor web con ESP32.</h1>");
+  client.println("<h1>Guadalupe Torres</h1>");
+  client.println("<h2><font color='#009900'>Webserver 1.0</font></h2>");
+  client.println("<h3>Práctica boton</h3>");
+  client.println("<br><br>");
+  client.println("<a href='on2'><button>Enciende LED</button></a>");
+  client.println("<a href='off2'><button>Apaga LED</button></a>");
+  client.println("<br><br>");
+  client.println(estado);
+  client.println("</font></center></body></html>");
+
+  Serial.print("Cliente desconectado: ");
+  Serial.println(client.remoteIP());
+
+  client.flush();
+  client.stop();
 }
