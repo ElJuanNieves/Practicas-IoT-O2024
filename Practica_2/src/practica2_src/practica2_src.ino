5f0f5223c0593ed0f2011a848680d27ffd8f7de3
Roberto Osorno <69988498+ElJuanNieves@users.noreply.github.com>
2024-09-17 18:53:33 -0600

    Update practica2_src.ino

--- Practica_2/src/practica2_src/practica2_src.ino
+++ Practica_2/src/practica2_src/practica2_src.ino
@@ -2,7 +2,7 @@
 #include <WiFi.h>
 
 const char* ssid = "IoT";
-const char* password = "it3s0IoT23";
+const char* password = "1t3s0IoT23";
 WiFiServer server(80);
 
 // Se tiene un LED incorporado en terminal 2, se puede usar otro GPIO
